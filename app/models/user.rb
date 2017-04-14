54624bd3ff1661eea8ad14f32ab9c98bbfd40b36
Louis Tran <loutran987@gmail.com>
2017-04-13 21:45:16 -0700

    Define methods to forget users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
 	before_save { self.email = email.downcase }
 	validates(:name, presence: true, length: {maximum: 50})
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -24,6 +25,16 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user
+  def forget
+    update_attributes(:remember_digest, nil)
   end
 
 end
