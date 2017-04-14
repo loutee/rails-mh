2dbefa78ab617dba4f46ff8cb988c91242b85661
Louis Tran <loutran987@gmail.com>
2017-04-13 21:35:19 -0700

    Create persistent sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
+      remember user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
